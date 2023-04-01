9ef89462b5f2f231c141d09477039993bef75c6e
mari zirakashvili <zirakashvili12@gmail.com>
2023-04-01 18:36:18 +0400

    uploading changes

---
+++ Database Example 3/HelloWorldDB/HelloWorldContext.cs
@@ -0,0 +1,59 @@
+﻿using Database_Example_3.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Example_3.HelloWorldDB
+{
+   public class HelloWorldContext : DbContext
+    {
+        public HelloWorldContext()
+        {
+        }
+
+        public HelloWorldContext(DbContextOptions<HelloWorldContext> options)
+        : base(options)
+        {
+        }
+
+  
+
+        public DbSet<Persons> Persons { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    => optionsBuilder.UseSqlServer("Server=.;Database=HelloWorld;persist security info=True;Integrated Security=True;TrustServerCertificate=True");
+        //("Server=192.168.0.10;Database=HRRAv3_ge-next_preprod;persist security info=True;user id=mzirakashvili;password=maR1Z!;TrustServerCertificate=True");
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Persons>(entity =>
+
+        {
+
+            entity.HasKey(e => e.PersonID);
+
+            entity.Property(e => e.LastName).IsRequired();
+
+            entity.Property(e => e.FirstName).IsRequired();
+
+            entity.Property(e => e.Address).IsRequired();
+
+            entity.Property(e => e.City).IsRequired();
+
+        });
+
+
+
+        }
+
+
+    }
+
+}
---
+++ Database Example 3/RelatedClasses.cs
@@ -0,0 +1,113 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Database_Example_3.Models;
+//using DependencyInjectionExample;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.Json;
+//using Database_Example_3.Models;
+//using System.Runtime.CompilerServices;
+
+//namespace Database_Example_3
+//{
+//    public class RelatedClasses
+//    {
+
+
+
+//        public void JobseekerRelatedClasses()
+//        {
+//            //        Assembly assembly = Assembly.GetExecutingAssembly();
+//            //        var typesWithJobseekerId = assembly.GetTypes()
+//            //            .Where(t => !t.IsAnonymousType() && t.GetProperties()
+//            //                .Any(p => p.Name == "JobseekerId"));
+
+//            //        foreach (var type in typesWithJobseekerId)
+//            //        {
+//            //            Console.WriteLine($"Type with JobseekerId property: {type.FullName}");
+//            //        }
+//            //    }
+//            //}
+
+//            //public static class TypeExtensions
+//            //{
+//            //    public static bool IsAnonymousType(this Type type)
+//            //    {
+//            //        return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+//            //               && type.IsGenericType && type.Name.Contains("AnonymousType")
+//            //               && (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
+//            //               && type.Attributes.HasFlag(TypeAttributes.NotPublic);
+//            //    }
+//            //}
+
+
+
+
+
+//            //{
+//            //        Assembly assembly = Assembly.GetExecutingAssembly();
+//            //        var typesWithJobseekerId = assembly.GetTypes()
+//            //.Where(t => t.GetProperties()
+//            //    .Any(p => p.Name == "JobseekerId"));
+
+//            //        foreach (var type in typesWithJobseekerId)
+//            //        {
+//            //            Console.WriteLine($"Type with JobseekerId property: {type.FullName}");
+
+//            //            // Check if the type has a relationship with the anonymous types
+//            //            var relatedTypes = assembly.GetTypes()
+//            //                .Where(t => t.GetProperties()
+//            //                    .Any(p => p.PropertyType == type && p.Name.EndsWith("Id")));
+
+//            //            foreach (var relatedType in relatedTypes)
+//            //            {
+//            //                Console.WriteLine($"  - Related type: {relatedType.FullName}");
+//            //            }
+//            //        }
+
+
+
+
+
+
+
+
+
+
+
+
+////aqedan
+
+
+//            Assembly assembly = Assembly.GetExecutingAssembly();
+//            var typesWithJobseekerId = assembly.GetTypes()
+//             .Where(t => t.GetProperties()
+//          .Any(p => p.Name == "JobseekerId"));
+
+//            foreach (var type in typesWithJobseekerId)
+//            {
+//                Console.WriteLine($"Class has a JobseekerId property:         {type.Name}         ");
+
+
+//                ////    check base types of the type
+//                //    var baseType = type.BaseType;
+//                //    while (baseType != null)
+//                //    {
+//                //        Console.WriteLine($" - Base type: {baseType.Name}");
+//                //        baseType = baseType.BaseType;
+//                //    }
+
+//            }
+
+
+
+//        }
+
+//    }
+//}
+    
+
